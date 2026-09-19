590f2a3d4cd51bedf7a08de019dc07d36a1fa147
Margarita Vovk <asfalt@192.168.1.6>
2026-09-19 16:23:21 +0200

    добавить метод удаления товара из чека

--- project_1.py
+++ project_1.py
@@ -25,3 +25,10 @@ class OnlineSalesRegisterCollector:
             self.__name_items.append(name)
             self.__number_items += 1
 
+    def delete_item_from_check(self, name):
+        if name not in self.__name_items:
+            raise NameError("Позиция отсутствует в чеке")
+        else: 
+            self.__name_items.remove(name)
+            self.__number_items -= 1
+
